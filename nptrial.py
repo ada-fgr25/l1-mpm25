0bbf81be1d8ca782f027e21426efd7c8c156538f
ada-fgr25 <francesco.remondi25@imperial.ac.uk>
2025-10-06 10:46:03 +0100

    New Function

--- nptrial.py
+++ nptrial.py
@@ -1,1 +1,15 @@
-import 
+import numpy as np
+def pip1(x):
+    return np.pi + x
+
+print(pip1(1.0))
+
+def pip2(x):
+    return np.pi + 2*x
+
+print(pip2(1.0))
+
+def pip3(x):
+  return np.pi + 3*x
+
+print(pip3(1.0))
